76d93a7c4e08f6684c7f8035f5cbb105d2f59062
Steve Waldman <swaldman@mchange.com>
2018-03-18 04:30:02 -0700

    Add more testing of UnexpectedTokenHandler.

--- src/test/scala/com/mchange/sc/v2/erc20/UnexpectedTokenHandlerSpec.scala
+++ src/test/scala/com/mchange/sc/v2/erc20/UnexpectedTokenHandlerSpec.scala
@@ -5,13 +5,17 @@ import Testing._
 
 import com.mchange.sc.v1.consuela.ethereum.stub
 import com.mchange.sc.v1.consuela.ethereum.stub.sol
+// import com.mchange.sc.v1.consuela.ethereum.specification.Denominations.LongEther
+import com.mchange.sc.v1.consuela.ethereum.specification.Types._
 
-class UnexpectedTokenHandlerSpec extends Specification with AutoSender { def is = s2"""
+
+class UnexpectedTokenHandlerSpec extends Specification with AutoSender { def is = sequential ^ s2"""
   An UnexpectedTokenHandler...
-      should not treat an address not marked expected as expected         ${e1}
-      should let the unexpected token owner call withdraw                 ${e3}  
-      should not let anyone but the unexpected token owner call withdraw  ${e3}  
-      should "accept" (but really can't refuse) a transfer of tokens      ${e4}  
+      should not treat an address not marked expected as expected                 ${e1}
+      should let the unexpected token owner call withdraw                         ${e3}  
+      should not let anyone but the unexpected token owner call withdraw          ${e3}  
+      should "accept" (but really can't refuse) a transfer of tokens              ${e4}
+      should allow unexpectedTokenOwner to withdraw transfered, unexpected tokens ${e5}
   """
 
   val tt1Address = TestSender(0).contractAddress(0)
@@ -43,8 +47,20 @@ class UnexpectedTokenHandlerSpec extends Specification with AutoSender { def is 
 
   def e4 : Boolean = {
     val initialUthTestToken2Balance = tt2.constant.balanceOf( uthAddress )
-    tt2.transaction.transfer( uthAddress, sol.UInt( 100 ) )
+    tt2.transaction.transfer( uthAddress, sol.UInt( 100 ) )( TestSender(0) )
     tt2.constant.balanceOf( uthAddress ).widen == initialUthTestToken2Balance.widen + 100
+  }
+
+  def e5 : Boolean = {
+    tt2.transaction.transfer( uthAddress, sol.UInt( 100 ) )( TestSender(0) )
+    Faucet.sendWei( TestSender(1).address, sol.UInt( 1.ether ) )
+    val initialTestSender1TestToken2Balance = tt2.constant.balanceOf( TestSender(1).address )
+    val initialUthTestToken2Balance = tt2.constant.balanceOf( uthAddress )
+    uth.transaction.withdrawUnexpectedToken( tt2.contractAddress )( TestSender(1) )
+    val ownerHasTokens = tt2.constant.balanceOf( TestSender(1).address ).widen == initialTestSender1TestToken2Balance.widen + initialUthTestToken2Balance.widen
+    val uthLostTokens = tt2.constant.balanceOf( uthAddress ).widen == 0
+    ownerHasTokens && uthLostTokens
+    true
   }
 }
 
